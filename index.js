217189170c24e52d95d1d92a50d06e63ccea768b
NeoMashego <mashegonoj@gmail.com>
2024-09-25 18:40:09 +0200

    changed fetch query, added catch for error with default background with author

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=balloon")
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=buildings")
     .then(res => res.json())
     .then(data => {
+        //console.log(data.urls.regular)
         document.body.style.backgroundImage = `url(${data.urls.full})`
         document.getElementById("author").textContent = `By: ${data.user.name}`
+    })
+    .catch(err => {
+        document.body.style.backgroundImage = `
+        url(https://images.unsplash.com/photo-1490122417551-6ee9691429d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MjcyODIwNzZ8&ixlib=rb-4.0.3&q=80&w=1080)`
+        document.getElementById("author").textContent = `By: Nichlas Andersen`
     })
 
 fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
