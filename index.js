88bb4979004c1427a5bf0d7cb17e656a8768f328
NeoMashego <mashegonoj@gmail.com>
2024-09-25 23:16:53 +0200

    created paragraphs to hold information about weather temp and city

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ navigator.geolocation.getCurrentPosition(position => {
         const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
         document.getElementById("weather").innerHTML = `
             <img src=${iconUrl} />
-            <p>${Math.round(data.main.temp)}℃</p>
-            <p>${data.name}</p>
+            <p class="weather-temp">${Math.round(data.main.temp)}℃</p>
+            <p class="weather-city">${data.name}</p>
         `
     })
     .catch(err => console.log(err))
