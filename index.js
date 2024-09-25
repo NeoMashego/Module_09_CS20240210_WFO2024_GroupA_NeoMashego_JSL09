0baacc688b2d472a0082c38cc8f569018b243a7f
NeoMashego <mashegonoj@gmail.com>
2024-09-25 18:00:49 +0200

    Created a fetch for dogecoin and displayed it on webpage

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(${data.urls.full})`
         document.getElementById("author").textContent = `By: ${data.user.name}`
     })
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById("crypto").innerHTML = `
+        <img src=${data.image.small}>
+        <span>${data.name}</span>`
+    })
